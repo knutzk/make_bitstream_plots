7add23ee9b40944212a53914e180659926fbf632
Knut Zoch <knut.zoch@gmail.com>
2018-11-01 12:31:12 +0100

    Updated lambda make_reduced_hist that returns just one histogram

--- util/plot.cc
+++ util/plot.cc
@@ -77,8 +77,6 @@ int main(int argc, char** argv) {
   const double pile_up_max = 62.5;
   const int n_bins_from_zero = std::floor((pile_up_max + 2.5)/5);
 
-  std::vector<std::unique_ptr<TH1D> > reduced_hists;
-
   // What we do here is the following: we look for all
   // module-granularity histograms that match the given wildcard.
   // For these histograms, we map from luminosity vs. bandwidth
@@ -87,7 +85,7 @@ int main(int argc, char** argv) {
   // getting histograms for all modules individually, we reduce
   // this infromation to _one_ histogram per given wildcard (e.g.
   // for layers).
-  auto add_to_reduced_hists = [&] (const std::string& wildcard, const std::string& title) {
+  auto make_reduced_hist = [&] (const std::string& wildcard, const std::string& title) {
     DirectoryParser parser{file, path, wildcard};
     TProfile prof{(title).c_str(), ("prof_" + title).c_str(), n_bins_from_zero, -2.5, pile_up_max, "s"};
     for (const auto& module : parser.modules) {
@@ -100,21 +98,23 @@ int main(int argc, char** argv) {
         prof.Fill(hist.getHisto()->GetBinCenter(i), hist.getHisto()->GetBinContent(i));
       }
     }
-    reduced_hists.emplace_back(std::unique_ptr<TH1D>(prof.ProjectionX()));
-    reduced_hists.back()->SetName(prof.GetName());
-    reduced_hists.back()->GetXaxis()->SetRangeUser(12.5, pile_up_max);
+    auto projection = std::unique_ptr<TH1D>(prof.ProjectionX());
+    projection->SetName(prof.GetName());
+    projection->GetXaxis()->SetRangeUser(12.5, pile_up_max);
+    return projection;
   };
-
-  add_to_reduced_hists("^L0", "L0");
-  add_to_reduced_hists("^L1", "L1");
-  add_to_reduced_hists("^L2", "L2");
-  add_to_reduced_hists("^ECA", "ECA");
-  add_to_reduced_hists("^ECC", "ECC");
-  add_to_reduced_hists("^LI.*_[AC][^(7|8)]_", "IBL2D");
-  add_to_reduced_hists("^LI.*_[AC][78]_", "IBL3D");
 
   // Plots for: total bit-stream usage
   // -------------------------------------------------------
+  std::vector<std::unique_ptr<TH1D> > reduced_hists;
+  reduced_hists.emplace_back(make_reduced_hist("^L0", "L0"));
+  reduced_hists.emplace_back(make_reduced_hist("^L1", "L1"));
+  reduced_hists.emplace_back(make_reduced_hist("^L2", "L2"));
+  reduced_hists.emplace_back(make_reduced_hist("^ECA", "ECA"));
+  reduced_hists.emplace_back(make_reduced_hist("^ECC", "ECC"));
+  reduced_hists.emplace_back(make_reduced_hist("^LI.*_[AC][^(7|8)]_", "IBL2D"));
+  reduced_hists.emplace_back(make_reduced_hist("^LI.*_[AC][78]_", "IBL3D"));
+
   HistStack stack{reduced_hists};
   stack.setXAxisTitle("Average #mu per lumi block");
   stack.setYAxisTitle("Average bandwidth usage");
